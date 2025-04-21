4124728d55b490848a715cb4aa60f1fb47823e7c
mohamedmadkour102 <140906202+mohamedmadkour102@users.noreply.github.com>
2025-04-21 20:29:41 +0200

    Add Controller Module.

--- MensCraft/Migrations/20250421173912_SeedCraftsmanAndCustomerRoles02.cs
+++ MensCraft/Migrations/20250421173912_SeedCraftsmanAndCustomerRoles02.cs
@@ -10,7 +10,7 @@ namespace MensCraft.Migrations
 		/// <inheritdoc />
 		protected override void Up(MigrationBuilder migrationBuilder)
 		{
-			// لازم تستخدم int للـ Id هنا بدل من Guid
+			
 			migrationBuilder.InsertData(
 				table: "AspNetRoles",
 				columns: new[] { "Id", "Name", "NormalizedName", "ConcurrencyStamp" },
